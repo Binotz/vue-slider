5b17322db62f056e291495aa6c4e9ff92c2aa725
Binotz <binotto.riccardo@gmail.com>
2022-06-24 15:19:44 +0200

    Exercise done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,83 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+// const slides = [
+//     {
+//         image: 'img/01.jpg',
+//         title: 'Svezia',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     },
+//     {
+//         image: 'img/02.jpg',
+//         title: 'Svizzera',
+//         text: 'Lorem ipsum.',
+//     },
+//     {
+//         image: 'img/03.jpg',
+//         title: 'Gran Bretagna',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     },
+//     {
+//         image: 'img/04.jpg',
+//         title: 'Germania',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+//     },
+//     {
+//         image: 'img/05.jpg',
+//         title: 'Paradise',
+//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+//     }
+// ];
 
-console.log(slides);
+//-----------------------
+//VueJS
+//-----------------------
+
+var app = new Vue(
+    {
+        el:'#app',
+        data:{
+            currentItem: 0,
+            slides:[
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ]
+        },
+        methods:{
+            showNextElement: function(){
+                if (this.currentItem >= this.slides.length -1){
+                    this.currentItem = 0;
+                } else {
+                    this.currentItem++;
+                }
+            },
+            previousNextElement: function(){
+                if (this.currentItem <= 0){
+                    this.currentItem = this.slides.length - 1;
+                } else {
+                    this.currentItem--;
+                }
+            }
+        },
+    }
+);
